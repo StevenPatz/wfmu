84b08f0f1d6ad323ed575bedbcd5e38cf8c3516b
Steven D. Patz <steven.patz@gmail.com>
2014-04-25 00:19:46 -0400

    Basic parsing.  Can get the link and the pubDate

--- parse.php
+++ parse.php
@@ -1,13 +1,23 @@
 <?php
-// The file test.xml contains an XML document with a root element
-// and at least an element /[root]/title.
-
-if (file_exists('DK.xml')) {
-    $xml = simplexml_load_file('DK.xml');
- 
-    print_r($xml);
-} else {
-    exit('Failed to open SK.xml.');
+$doc = new DOMDocument();
+$doc->load('DK.xml');
+$arrFeeds = array();
+foreach ($doc->getElementsByTagName('item') as $node) {
+    $itemRSS = array ( 
+        'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+        'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+        'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+        'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+    );
+    array_push($arrFeeds, $itemRSS);
 }
+$links = count($arrFeeds);
+for ($x=0;$x<$links;$x++) {
+  print $arrFeeds[$x]['link'];
+  print "\n";
+  print $arrFeeds[$x]['date'];
+  print "\n";
+}
+//print $arrFeeds[5]['link'];
 ?>
 
